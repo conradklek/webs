887171a15162802a6fc54ff5e0133b4caf379cff
Conrad Klek <conradklek@gmail.com>
2025-08-14 12:32:50 -0500

    increment create-webs-app version

--- packages/create-webs-app/template/src/app/index.js
+++ packages/create-webs-app/template/src/app/index.js
@@ -1,12 +1,19 @@
 export default {
   name: "Home",
-  state: () => ({
-    message: "Welcome to webs.js"
-  }),
+  state() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
   template: `
     <div class="p-8 flex flex-col items-start justify-start gap-2 min-h-screen">
       <h1 class="font-medium">@conradklek/webs</h1>
-      <p>{{ message }}</p>
+      <button type="button" @click="increment" class="cursor-pointer active:opacity-50">This button has been clicked {{ count }} time{{ count === 1 ? '' : 's' }}!</button>
       <p>Edit <code class="text-sm">src/app/index.js</code> to get started.</p>
     </div>
   `,
