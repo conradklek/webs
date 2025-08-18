082bbd277043eeee8b6d3e7643b7e3d200712410
Conrad Klek <conradklek@gmail.com>
2025-08-18 00:03:42 -0500

    Remove source-map-explorer

--- packages/demo/.tmp/app.js
+++ packages/demo/.tmp/app.js
@@ -1,14 +1,10 @@
 import { create_router } from "@conradklek/webs";
-import { use_logger } from "../src/use/logger.js";
-import { use_auth } from "../src/use/auth.js";
-import Profile from "../src/app/profile.js";
-import Home from "../src/app/index.js";
-import Signup from "../src/app/signup.js";
-import Login from "../src/app/login.js";
 
-const routes = { "/profile": { component: Profile, middleware: [use_logger, use_auth] },
-  "/": { component: Home, middleware: [use_logger] },
-  "/signup": { component: Signup, middleware: [use_logger] },
-  "/login": { component: Login, middleware: [use_logger] } };
+const routes = { 
+  "/profile": () => import("../src/app/profile.js"),
+  "/": () => import("../src/app/index.js"),
+  "/signup": () => import("../src/app/signup.js"),
+  "/login": () => import("../src/app/login.js") 
+};
 
 create_router(routes);
